3e1ea455c8c6848e20ff283db6726c8a4edd2741
beneditojuniorbr <beneditosouzajr@gmail.com>
2026-04-24 19:48:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -110,8 +110,8 @@
                 
                 // Escolhe o ícone baseado no tipo
                 if (type === 'Ebook') {
-                    // Ícone de Livro Aberto Simples
-                    iconPath = `<path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>`;
+                    // Ícone de Livro Aberto com Bordas Arredondadas (Premium)
+                    iconPath = `<path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" /><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />`;
                 } else if (type === 'Mentoria') {
                     iconPath = `<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/>`;
                 } else { // Curso
@@ -119,7 +119,7 @@
                 }
 
                 const svg = `
-                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 24 24" fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round">
                         <defs>
                             <linearGradient id="grad-${type}" x1="0%" y1="0%" x2="100%" y2="100%">
                                 <stop offset="0%" style="stop-color:#ff005c;stop-opacity:1" />
@@ -127,7 +127,7 @@
                             </linearGradient>
                         </defs>
                         <rect x="0" y="0" width="24" height="24" fill="#ffffff" />
-                        <g stroke="url(#grad-${type})" transform="translate(6, 6) scale(0.5)">
+                        <g stroke="url(#grad-${type})" transform="translate(7.8, 7.8) scale(0.35)">
                             ${iconPath}
                         </g>
                     </svg>
